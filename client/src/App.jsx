98fe2c196f7bf5c9cc5ed9d50d5b12bfa2a17e55
Shetty <shwshett@bmc.com>
2023-01-19 16:14:19 +0530

    Add host form

---
+++ client/src/App.jsx
@@ -0,0 +1,68 @@
+import logo from './logo.svg';
+import './App.css';
+import FormInput from './components/forminput';
+import { useState } from 'react';
+
+function App() {
+  const [values,setValues]= useState({
+    hostname:"",
+    username:"",
+    password:"",
+  });
+
+  const inputs=[
+    {
+      id:1,
+      name: "hostname",
+      type: "text",
+      placeholder: "Hostname",
+      label: "Hostname",
+      errormsg: "Cannot be empty",
+      required: true
+    },
+    {
+      id:2,
+      name: "username",
+      type: "text",
+      placeholder: "Username",
+      label: "Username",
+      errormsg: "Cannot be empty",
+      required: true
+    },
+    {
+      id:3,
+      name: "password",
+      type: "password",
+      placeholder: "Password",
+      label: "Password",
+      errormsg: "Must contain at least 1 char and 1 number",
+      required: true,
+      pattern: "^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$"
+    },
+
+  ]
+
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+
+  }
+
+  const onChange=(e)=>{
+    setValues({...values,[e.target.name]: e.target.value});
+  }
+  
+  console.log(values);
+
+  return (
+    <div className='app'>
+      <form onSubmit={handleSubmit}>
+        <h1>Add host</h1>
+        {inputs.map((input)=>( 
+        <FormInput key={input.id} {...input} value={values[input.name]} onChange={onChange}/>
+        ))}
+        <button>Submit</button>
+      </form>
+    </div>  );
+}
+
+export default App;
